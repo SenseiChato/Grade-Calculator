5f44ebe88d0c4469d04c6eafd2dc1154f2d65b3a
SenseiChato <45278055+SenseiChato@users.noreply.github.com>
2018-11-22 20:34:33 -0700

    Add files via upload

---
+++ LAB2_GradeCalculator (Mas Nuevo)/Grade Calculator/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Grade_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtMidTermLetterGrade = new System.Windows.Forms.TextBox();
+            this.txtMidTermGrade = new System.Windows.Forms.TextBox();
+            this.txtFinalExamGrade = new System.Windows.Forms.TextBox();
+            this.txtFinalExamLetterGrade = new System.Windows.Forms.TextBox();
+            this.txtFinalGrade = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lstQuizzes = new System.Windows.Forms.ListBox();
+            this.txtLetterGrade1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtQuiz = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Quiz Marks:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(27, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Midterm:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(27, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Final Exam:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnCalculate.Location = new System.Drawing.Point(90, 246);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(135, 61);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(87, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "GRADE";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(329, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "LETTER GRADE";
+            // 
+            // txtMidTermLetterGrade
+            // 
+            this.txtMidTermLetterGrade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMidTermLetterGrade.Location = new System.Drawing.Point(359, 90);
+            this.txtMidTermLetterGrade.Name = "txtMidTermLetterGrade";
+            this.txtMidTermLetterGrade.ReadOnly = true;
+            this.txtMidTermLetterGrade.Size = new System.Drawing.Size(51, 20);
+            this.txtMidTermLetterGrade.TabIndex = 19;
+            this.txtMidTermLetterGrade.TabStop = false;
+            // 
+            // txtMidTermGrade
+            // 
+            this.txtMidTermGrade.Location = new System.Drawing.Point(143, 169);
+            this.txtMidTermGrade.Name = "txtMidTermGrade";
+            this.txtMidTermGrade.Size = new System.Drawing.Size(51, 20);
+            this.txtMidTermGrade.TabIndex = 4;
+            // 
+            // txtFinalExamGrade
+            // 
+            this.txtFinalExamGrade.Location = new System.Drawing.Point(143, 204);
+            this.txtFinalExamGrade.Name = "txtFinalExamGrade";
+            this.txtFinalExamGrade.Size = new System.Drawing.Size(51, 20);
+            this.txtFinalExamGrade.TabIndex = 5;
+            // 
+            // txtFinalExamLetterGrade
+            // 
+            this.txtFinalExamLetterGrade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFinalExamLetterGrade.Location = new System.Drawing.Point(359, 125);
+            this.txtFinalExamLetterGrade.Name = "txtFinalExamLetterGrade";
+            this.txtFinalExamLetterGrade.ReadOnly = true;
+            this.txtFinalExamLetterGrade.Size = new System.Drawing.Size(51, 20);
+            this.txtFinalExamLetterGrade.TabIndex = 22;
+            this.txtFinalExamLetterGrade.TabStop = false;
+            // 
+            // txtFinalGrade
+            // 
+            this.txtFinalGrade.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtFinalGrade.Location = new System.Drawing.Point(321, 268);
+            this.txtFinalGrade.Name = "txtFinalGrade";
+            this.txtFinalGrade.ReadOnly = true;
+            this.txtFinalGrade.Size = new System.Drawing.Size(51, 20);
+            this.txtFinalGrade.TabIndex = 23;
+            this.txtFinalGrade.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(297, 246);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 15);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "FINAL GRADE";
+            // 
+            // lstQuizzes
+            // 
+            this.lstQuizzes.FormattingEnabled = true;
+            this.lstQuizzes.Location = new System.Drawing.Point(148, 11);
+            this.lstQuizzes.Name = "lstQuizzes";
+            this.lstQuizzes.Size = new System.Drawing.Size(150, 134);
+            this.lstQuizzes.TabIndex = 28;
+            // 
+            // txtLetterGrade1
+            // 
+            this.txtLetterGrade1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLetterGrade1.Location = new System.Drawing.Point(359, 50);
+            this.txtLetterGrade1.Name = "txtLetterGrade1";
+            this.txtLetterGrade1.ReadOnly = true;
+            this.txtLetterGrade1.Size = new System.Drawing.Size(51, 20);
+            this.txtLetterGrade1.TabIndex = 6;
+            this.txtLetterGrade1.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(30, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(86, 28);
+            this.btnAdd.TabIndex = 29;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtQuiz
+            // 
+            this.txtQuiz.Location = new System.Drawing.Point(30, 82);
+            this.txtQuiz.Name = "txtQuiz";
+            this.txtQuiz.Size = new System.Drawing.Size(70, 20);
+            this.txtQuiz.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 271);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(66, 36);
+            this.btnClear.TabIndex = 30;
+            this.btnClear.Text = "C&lear:";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(502, 319);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtQuiz);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstQuizzes);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtFinalGrade);
+            this.Controls.Add(this.txtFinalExamLetterGrade);
+            this.Controls.Add(this.txtFinalExamGrade);
+            this.Controls.Add(this.txtMidTermLetterGrade);
+            this.Controls.Add(this.txtMidTermGrade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtLetterGrade1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Grade Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMidTermLetterGrade;
+        private System.Windows.Forms.TextBox txtMidTermGrade;
+        private System.Windows.Forms.TextBox txtFinalExamGrade;
+        private System.Windows.Forms.TextBox txtFinalExamLetterGrade;
+        private System.Windows.Forms.TextBox txtFinalGrade;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox lstQuizzes;
+        private System.Windows.Forms.TextBox txtLetterGrade1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtQuiz;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ LAB2_GradeCalculator (Mas Nuevo)/Grade Calculator/Form1.cs
@@ -0,0 +1,353 @@
+﻿/**********************************
+ ***Author: Del_Aguila_Daniel
+ ***Date: October, 18th 2018
+ **********************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grade_Calculator
+{
+    public partial class Form1 : Form
+    {
+        const int MAX = 10;
+        string[] quizNumber = { "Quiz 1", "Quiz 2", "Quiz 3", "Quiz 4", "Quiz 5", "Quiz 6", "Quiz 7", "Quiz 8", "Quiz 9", "Quiz 10"};
+        double[] qMarks = new double[MAX];
+        int quizCounter = 0;
+        //double midtermGrade;
+        //double finalExamGrade;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //The following will add quizzes into the list box
+            double scores;
+            double[] subtotal = new double[MAX];
+            //int arr = 0;
+
+            try
+            {
+                scores = Convert.ToDouble(txtQuiz.Text);
+                qMarks[quizCounter] = scores;
+                quizCounter++;
+                GetQuizzList(quizNumber, qMarks, quizCounter);  //Calling the add quizzes method
+            }
+            //Will prevent the user from entering more than 10 quizzes
+            catch
+                (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("No more quizzes allowed!", "Error", MessageBoxButtons.OK);
+                this.txtQuiz.Enabled = false;   //This will de-activate the Quiz textbox after maximum no. of quizzes entered
+                this.btnAdd.Enabled = false;    //This will de-activate the button add after maximum no. of quizzes entered
+            }
+
+            this.txtQuiz.Clear();
+        }
+        
+        //The following method will add quizzes entered
+        private void GetQuizzList(string[] n, double[] p, int count)
+        {
+            lstQuizzes.Items.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                lstQuizzes.Items.Add(n[i] + "\t" + p[i].ToString());
+            }
+        }
+
+        //public double FindLowest()
+        //{
+        //    double qMarks = 0;
+        //    for (int i = 0; i < qMarks; i++)
+        //    {
+        //        qMarks += quizCounter[1] * quizNumber[i]
+        //    }
+        //}
+
+        //The following method is used to make the math to attain the percentage of the quizzes.
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            ////////Variables for the Grade textboxes
+            ////double grade1;
+            ////double grade2;
+            ////double grade3;
+            double quizNumber1 = Convert.ToDouble(txtMidTermGrade.Text);
+            double quizNumber2 = Convert.ToDouble(txtFinalExamGrade.Text); ;
+            //double quizNumber3 = Convert.ToDouble(txtQuiz.Text); ;
+            double midtermGrade;
+            double finalExamGrade;
+
+            ////////Variables for the Letter Grade textboxes
+            //string letterGrade1;
+            //string letterGrade2;
+            //string letterGrade3;
+            //string midtermLetterGrade;
+            //string finalExamLetterGrade;
+
+            ////////Converting the double variables to be written in textboxes
+            ////double grade1 = Convert.ToDouble(txtQuiz.Text);
+            ////double grade2 = Convert.ToDouble(txtQuiz.Text);
+            ////double grade3 = Convert.ToDouble(txtQuiz.Text);
+            midtermGrade = Convert.ToDouble(txtMidTermGrade.Text);
+            finalExamGrade = Convert.ToDouble(txtFinalExamGrade.Text);
+
+            double gradePct1 = (double)quizNumber1 * 15 / 100;
+            double gradePct2 = (double)quizNumber2 * 15 / 100;
+            //double gradePct3 = (double)quizNumber3 * 15 / 100;
+
+            //////Formulas for obtaining Grade percentages
+            //double gradePct1 = (double)grade1 * 15 / 100;
+            //double gradePct2 = (double)grade2 * 15 / 100;
+            //double gradePct3 = (double)grade3 * 15 / 100;
+
+            //double midtermPct = (double)midtermGrade * 30 / 100;
+            //double finalExamPct = (double)finalExamGrade * 40 / 100;
+
+            ////////Variable that drops the lowest of the three quizzes
+            //string lowestGradeDropped = "N/A";
+
+            double quizSum;
+            double midtermPct;
+            double finalExamPct;
+            quizSum = 0;
+
+            for (int i = 0; i <= 9; i++)
+            {
+                quizSum = quizSum + qMarks[i];
+                midtermGrade = midtermGrade * 30 / 100;
+                finalExamGrade = finalExamGrade * 40 / 100;
+            }
+
+            quizSum = quizSum * 3.3333 / 100;
+            //quizSum = quizSum * 3.3333 / 100 / 10;
+            midtermPct = (double)midtermGrade * 30 / 100;
+            finalExamPct = (double)finalExamGrade * 40 / 100;
+
+            txtFinalGrade.Text = quizSum.ToString("p");
+
+            //Formulas for obtaining the Final Grade
+            //double finalGrade1SumPct = gradePct1 + gradePct2 + midtermPct + finalExamPct;
+            //double finalGrade2SumPct = gradePct1 + gradePct3 + midtermPct + finalExamPct;
+            //double finalGrade3SumPct = gradePct1 + gradePct2 + midtermPct + finalExamPct;
+
+            //Formulas for obtaining the Final Grade
+            //double finalGrade1SumPct = gradePct2 + gradePct3 + midtermPct + finalExamPct;
+            //double finalGrade2SumPct = gradePct1 + gradePct3 + midtermPct + finalExamPct;
+            //double finalGrade3SumPct = gradePct1 + gradePct2 + midtermPct + finalExamPct;
+
+            //If/Else statements for the 1st Quizz that displays the Grade% and Letter Grade achieved
+            //if (grade1 >= 90 && grade1 <= 100)
+            //{
+            //    letterGrade1 = "A";                     //The Letter Grade assigned to the Grade score
+            //    txtGrade1Pct.Text = gradePct1.ToString("N2"); //It restricts the value by two decimals
+            //}
+
+            //else if (grade1 >= 80 && grade1 < 90)
+            //{
+            //    letterGrade1 = "B";
+            //    txtGrade1Pct.Text = gradePct1.ToString("N2");
+            //}
+
+            //else if (grade1 >= 70 && grade1 < 80)
+            //{
+            //    letterGrade1 = "C";
+            //    txtGrade1Pct.Text = gradePct1.ToString("N2");
+            //}
+
+            //else if (grade1 >= 60 && grade1 < 70)
+            //{
+            //    letterGrade1 = "D";
+            //    txtGrade1Pct.Text = gradePct1.ToString("N2");
+            //}
+
+            //else
+            //{
+            //    letterGrade1 = "F";
+            //    txtGrade1Pct.Text = gradePct1.ToString("N2");
+            //}
+
+            ////If/Else statements for the 2nd Quizz that displays the Grade% and Letter Grade achieved
+            //if (grade2 >= 90 && grade2 <= 100)
+            //{
+            //    letterGrade2 = "A";
+            //    txtGrade2Pct.Text = gradePct2.ToString("N2");
+            //}
+
+            //else if (grade2 >= 80 && grade2 < 90)
+            //{
+            //    letterGrade2 = "B";
+            //    txtGrade2Pct.Text = gradePct2.ToString("N2");
+            //}
+
+            //else if (grade2 >= 70 && grade2 < 80)
+            //{
+            //    letterGrade2 = "C";
+            //    txtGrade2Pct.Text = gradePct2.ToString("N2");
+            //}
+
+            //else if (grade2 >= 60 && grade2 < 70)
+            //{
+            //    letterGrade2 = "D";
+            //    txtGrade2Pct.Text = gradePct2.ToString("N2");
+            //}
+
+            //else
+            //{
+            //    letterGrade2 = "F";
+            //    txtGrade2Pct.Text = gradePct2.ToString("N2");
+            //}
+
+            ////If/Else statements for the 3rd Quizz that displays the Grade% and Letter Grade achieved
+            //if (grade3 >= 90 && grade3 <= 100)
+            //{
+            //    letterGrade3 = "A";
+            //    txtGrade3Pct.Text = gradePct3.ToString("N2");
+            //}
+
+            //else if (grade3 >= 80 && grade3 < 90)
+            //{
+            //    letterGrade3 = "B";
+            //    txtGrade3Pct.Text = gradePct3.ToString("N2");
+            //}
+
+            //else if (grade3 >= 70 && grade3 < 80)
+            //{
+            //    letterGrade3 = "C";
+            //    txtGrade3Pct.Text = gradePct3.ToString("N2");
+            //}
+
+            //else if (grade3 >= 60 && grade3 < 70)
+            //{
+            //    letterGrade3 = "D";
+            //    txtGrade3Pct.Text = gradePct3.ToString("N2");
+            //}
+
+            //else
+            //{
+            //    letterGrade3 = "F";
+            //    txtGrade3Pct.Text = gradePct3.ToString("N2");
+            //}
+
+            ////If/Else statements for the Midterm that displays the Grade% and Letter Grade achieved
+            //if (midtermGrade >= 90 && midtermGrade <= 100)
+            //{
+            //    midtermLetterGrade = "A";
+            //    txtMidTermPct.Text = midtermPct.ToString("N2");
+            //}
+
+            //else if (midtermGrade >= 80 && midtermGrade < 90)
+            //{
+            //    midtermLetterGrade = "B";
+            //    txtMidTermPct.Text = midtermPct.ToString("N2");
+            //}
+
+            //else if (midtermGrade >= 70 && midtermGrade < 80)
+            //{
+            //    midtermLetterGrade = "C";
+            //    txtMidTermPct.Text = midtermPct.ToString("N2");
+            //}
+
+            //else if (midtermGrade >= 60 && midtermGrade < 70)
+            //{
+            //    midtermLetterGrade = "D";
+            //    txtMidTermPct.Text = midtermPct.ToString("N2");
+            //}
+
+            //else
+            //{
+            //    midtermLetterGrade = "F";
+            //    txtMidTermPct.Text = midtermPct.ToString("N2");
+            //}
+
+            ////If/Else statements for the Final Exam that displays the Grade% and Letter Grade achieved
+            //if (finalExamGrade >= 90 && finalExamGrade <= 100)
+            //{
+            //    finalExamLetterGrade = "A";
+            //    txtFinalExamPct.Text = finalExamPct.ToString("N2");
+            //}
+
+            //else if (finalExamGrade >= 80 && finalExamGrade < 90)
+            //{
+            //    finalExamLetterGrade = "B";
+            //    txtFinalExamPct.Text = finalExamPct.ToString("N2");
+            //}
+
+            //else if (finalExamGrade >= 70 && finalExamGrade < 80)
+            //{
+            //    finalExamLetterGrade = "C";
+            //    txtFinalExamPct.Text = finalExamPct.ToString("N2");
+            //}
+
+            //else if (finalExamGrade >= 60 && finalExamGrade < 70)
+            //{
+            //    finalExamLetterGrade = "D";
+            //    txtFinalExamPct.Text = finalExamPct.ToString("N2");
+            //}
+
+            //else
+            //{
+            //    finalExamLetterGrade = "F";
+            //    txtFinalExamPct.Text = finalExamPct.ToString("N2");
+            //}
+
+            //If/Else statements and formulas for the Lowest Grade Dropped
+            //if (grade1 <= grade2 && grade1 < grade3)
+            //{
+            //    lowestGradeDropped = "N/A"; //Lowest grade % dropped, displaying it as N/A (non applicable)
+            //    letterGrade1 = "N/A";       //Lowest letter grade dropped, displaying it as N/A (non applicable)
+            //    txtGrade1Pct.Text = lowestGradeDropped.ToString();
+            //    txtFinalGrade.Text = finalGrade1SumPct.ToString();
+            //}
+
+            //else if (grade2 <= grade1 && grade2 < grade3)
+            //{
+            //    lowestGradeDropped = "N/A";
+            //    letterGrade2 = "N/A";
+            //    txtGrade2Pct.Text = lowestGradeDropped.ToString();
+            //    txtFinalGrade.Text = finalGrade2SumPct.ToString();
+            //}
+
+            //else
+            //{
+            //    lowestGradeDropped = "N/A";
+            //    letterGrade3 = "N/A";
+            //    txtGrade3Pct.Text = lowestGradeDropped.ToString();
+            //    txtFinalGrade.Text = finalGrade3SumPct.ToString();
+            //}
+
+            ////Focuses the cursor back to Quiz#1 after the Calculate button has been used
+            //txtQuiz1.Focus();
+
+            //Takes the variables "...letterGrade..." and displays them into textboxes 
+            //txtLetterGrade1.Text = letterGrade1.ToString();
+            //txtLetterGrade2.Text = letterGrade2.ToString();
+            //txtLetterGrade3.Text = letterGrade3.ToString();
+            //txtMidTermLetterGrade.Text = midtermLetterGrade.ToString();
+            //txtFinalExamLetterGrade.Text = finalExamLetterGrade.ToString();
+
+        }
+        
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            //The following clears the values entered
+            lstQuizzes.Items.Clear();
+            txtMidTermGrade.Clear();
+            txtFinalExamGrade.Clear();
+            txtFinalGrade.Clear();
+            quizCounter = 0;
+            //The following resets the quiz textbox and add button so they can be used anew
+            this.txtQuiz.Enabled = true;
+            this.btnAdd.Enabled = true;
+        }
+    }
+}
